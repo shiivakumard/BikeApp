1a8d28bd623ec926d92811503e5c4fe94f188830
shivakumar <shiivakumar.d@gmail.com>
2024-10-09 10:20:33 +0530

    git changes

---
+++ bikeapp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <footer>
+        <div>
+          <h5>whatsupp creadsdd
+            
+          </h5>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
