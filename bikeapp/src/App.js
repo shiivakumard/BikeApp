b5a114901fbfc59801de77bbb7a8363b2cf45be9
shivakumar <shiivakumar.d@gmail.com>
2024-10-17 18:22:02 +0530

    adding routes to application

--- bikeapp/src/App.js
+++ bikeapp/src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Log from './pages/Log';
+import Dashboard from './pages/Dashboard';
+import Linkk from './utill/Linkk';
 
 
 function App() {
   return (
     <div className="App">
+      <Linkk/>
      <Routes>
-      <Route path='/' element={<Log/>}/>
+      <Route path='log' element={<Log/>}/>
+      <Route path='/' element={<Dashboard/>}/>
      </Routes>
      
     </div>
---
+++ bikeapp/src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard(){
+    return(
+        <div>
+            <h4>login page</h4>
+
+        </div>
+    )
+}
